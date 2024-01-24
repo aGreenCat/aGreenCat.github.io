9bc74b83b6f9ec1f6644457fb56d8a3fe216e050
Tedd Lee <tedd0328@gmail.com>
2024-01-24 17:27:58 -0500

    Use Static Export

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,8 +1,22 @@
+import Image from 'next/image'
+
 const page = () => {
   return (
-    <main>
-        About
-    </main>
+    <div className="flex flex-row flex-wrap w-[640px] mx-auto justify-between">
+        <Image src="/Rectangle3.png" alt="Me" width={300} height={300}/>
+        <div className="w-[300px]">
+            <div>Hey! my name is Tedd Lee, and I’m a first-year undergraduate student attending Macaulay Honors College
+                at
+                Hunter College (CUNY). In middle school, my computer science teacher introduced me to Scratch,
+                hackathons,
+                game jams, and I’ve been hooked ever since.
+            </div>
+            <div>After years of exploration, I have grown to appreciate the creative freedom and enjoy the problem
+                solving in this broad field of CS. Thus, I am now pursuing a computer science degree and building a
+                career for the future.
+            </div>
+        </div>
+    </div>
   )
 }
 
