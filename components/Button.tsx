06ed61fa94bd3b2d2fab1dbd184689ad8e51cdd1
Tedd Lee <tedd0328@gmail.com>
2024-04-05 23:00:10 -0400

    Add Project Thumbnails

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,7 +1,11 @@
+import Link from "next/link";
+
 export const Button = ({text, link} : {text : string, link : string}) => {
     return (
-        <button className="bg-pink-600 hover:bg-pink-800 text-white font-light lg:font-medium py-2 px-4 rounded-md w-min h-min">
-            {text}
-        </button>
+        <Link href={link}>
+            <button className="bg-pink-600 hover:bg-pink-800 text-white font-light lg:font-medium py-2 px-4 rounded-md w-min h-min">
+                {text}
+            </button>
+        </Link>
     )
 }
--- components/Card.tsx
+++ components/Card.tsx
@@ -16,7 +16,7 @@ interface CardProps {
 const Card = ({title, description, image, link, month, year} : CardProps) => {
     return (
         <div className="flex flex-row bg-slate-50 bg-opacity-5 hover:bg-opacity-10 transition-all duration-500 ease-out p-[35px] lg:p-[55px] rounded-2xl flex-wrap justify-between gap-4 lg:gap-0 mb-10">
-            <Image src={image} alt="grey placeholder" width={200} height={150} className="rounded-xl"></Image>
+            <Image src={image} alt="grey placeholder" width={200} height={170} className="rounded-xl"></Image>
             <div className="flex flex-col justify-between w-[250px] gap-3 lg:gap-0">
                 <Title text={title}/>
                 <Description text={description}/>
